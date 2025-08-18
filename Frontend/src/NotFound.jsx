1ad66a5603c87f38723b3f091b3509b440b5c2d5
Sachin Garg <s32314800@gmail.com>
2025-08-18 18:11:56 +0530

    Add files via upload

---
+++ Frontend/src/NotFound.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Navbar from './components/landingPage/Navbar';
+const NotFound = () => {
+    const navigate = useNavigate();
+    useEffect(()=>{
+        // navigate('/login');
+    },[])
+  return (
+    <>
+        <Navbar/>
+        <h1>Not Found</h1>
+    </>
+  )
+}
+
+export default NotFound;
