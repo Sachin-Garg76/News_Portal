4eb1b9afe8a05a6c89052e391b06e09972ca0a75
Sachin Garg <s32314800@gmail.com>
2026-09-07 16:54:15 +0530

    Fix frontend import casing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,15 +7,15 @@ import './App.css';
 // landing page component 
 import Home from './components/landingpage/Home';
 import SignIn from './components/landingpage/SignIn';
-import AboutUs from './components/landingPage/AboutUs';
+import AboutUs from './components/landingpage/AboutUs';
 import NewsDetails from './components/landingpage/NewsDetail';
 import NewsCategory from './components/landingpage/NewsCategory';
 import ImageGaralley from './components/landingpage/ImageGaralley';
 import Latestvideos from './components/landingpage/Latestvideos';
 import ContactUs from './components/landingpage/ContactUs';
 import UserRegister from './components/landingpage/UserRegister';
-import Footer from './components/landingPage/Footer';
-import TopNews from './components/landingPage/TopNews';
+import Footer from './components/landingpage/Footer';
+import TopNews from './components/landingpage/TopNews';
 // user component 
 import PostNews from './components/user/PostNews';
 import YourNews from './components/user/YourNews';
